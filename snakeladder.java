12df0db662bf916f0354f5fd4d72a0a667fc0c8f
unknown <naikmahesh.naik@gmail.com>
2021-02-22 15:22:20 +0530

    refactor use Random function check option

--- snakeladder.java
+++ snakeladder.java
@@ -1,5 +1,12 @@
 public class snakeladder {
+	
+	
+	static final int NoPlay = 0;
+	static final int Ladder = 1;
+	static final int Snake = 2;
+	static int Sum = 0;
 
+	
 	public static void main(String[] args) {
 	int die;
 	//using random function
@@ -7,7 +14,25 @@ public class snakeladder {
 
 		System.out.println("start position is zero");
 		System.out.println("The Player rolls the die no is "+die);
-	}
+	
+		int opt = (int) Math.floor(Math.random() * 10) % 3;
+
+		if (opt == Ladder) {
+			Sum = Sum + die;
+			System.out.println("Ladder");
+}
+		else if (opt == Snake) {
+
+			Sum = Sum - die;
+			System.out.println("Snake");
+}
+else {
+			System.out.println("No play");
+
+}
+
+}
+
 
 }
 
