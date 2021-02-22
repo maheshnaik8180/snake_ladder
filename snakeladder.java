117b5b65171433ba8d813d27656af1fa89ef3344
unknown <naikmahesh.naik@gmail.com>
2021-02-22 14:49:08 +0530

    refactor use random function get the number betwwen 1 to 6

--- snakeladder.java
+++ snakeladder.java
@@ -1,7 +1,13 @@
 public class snakeladder {
 
 	public static void main(String[] args) {
+	int die;
+	//using random function
+	die=(int)(Math.random()*6) + 1;
 
 		System.out.println("start position is zero");
+		System.out.println("The Player rolls the die no is "+die);
 	}
+
 }
+
